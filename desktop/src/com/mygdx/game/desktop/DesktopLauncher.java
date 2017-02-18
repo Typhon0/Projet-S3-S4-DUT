d27c93bf77e73807c05256db347c3d7f70b9fc78
nico59600 <nicolasgeris@gmail.com>
2017-02-18 18:43:14 +0100

    modif

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -10,10 +10,10 @@ import com.mygdx.game.model.HexTile;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = Gdx.app.getGraphics().getWidth();//1920;//2;//Gdx.app.getGraphics().getWidth(); //1920/2;
-		config.height = Gdx.app.getGraphics().getHeight();//1080;//2;//Gdx.app.getGraphics().getHeight(); //1080/2;
-//		config.width = 1920;//1920;//2;//Gdx.app.getGraphics().getWidth(); //1920/2;
-//		config.height = 1080;//1080;//2;//Gdx.app.getGraphics().getHeight(); //1080/2;
+//		config.width = Gdx.app.getGraphics().getWidth();//1920;//2;//Gdx.app.getGraphics().getWidth(); //1920/2;
+//		config.height = Gdx.app.getGraphics().getHeight();//1080;//2;//Gdx.app.getGraphics().getHeight(); //1080/2;
+		config.width = 1920;//1920;//2;//Gdx.app.getGraphics().getWidth(); //1920/2;
+		config.height = 1080;//1080;//2;//Gdx.app.getGraphics().getHeight(); //1080/2;
 		new LwjglApplication(new Draw(), config);
 		System.out.println("test");
 
