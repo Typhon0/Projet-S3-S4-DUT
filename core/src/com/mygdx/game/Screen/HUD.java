659e6036b867bc7df4bbf8c92c1a6c6f69f0213e
Typhon <loic.sculier@hotmail.fr>
2017-03-17 21:53:26 +0100

    ajout listener des button du HUD
    ajout listener des button du HUD(

--- core/src/com/mygdx/game/Screen/HUD.java
+++ core/src/com/mygdx/game/Screen/HUD.java
@@ -2,10 +2,13 @@ package com.mygdx.game.Screen;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
@@ -18,13 +21,12 @@ public class HUD {
     public Stage stage;
     private Skin skin;
 
-    public HUD(SpriteBatch sb)  {
+    public HUD(SpriteBatch sb) {
 
         skin = new Skin(Gdx.files.internal("ui/glassy-ui.json"));
 
 
         stage = new Stage(new ExtendViewport(1920, 1080));
-        //Gdx.input.setInputProcessor(stage);
 
         Table table = new Table();
         table.setSize(1920, 1080);
@@ -56,19 +58,89 @@ public class HUD {
 
         final Button pions = new Button(skin, "round");
         table.add(pions).size(170, 170).padRight(1500);
+
         final Button quit = new Button(skin, "exit");
         table.add(quit).size(170, 170);
         table.row();
 
-        final Button passtour = new Button(skin, "round");
-        table.add(passtour).size(170, 170).padRight(1500);
+        final Button passertour = new Button(skin, "round");
+        table.add(passertour).size(170, 170).padRight(1500);
         table.row();
 
-
-        //  table.pack();
-
-
         stage.addActor(table);
+
+        //Listener Bouton piocher
+        piocher.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+
+
+            }
+        });
+
+
+        //Listener Bouton statistique
+        stat.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+
+
+            }
+        });
+
+        //Listener Bouton echange
+        echange.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+
+
+            }
+        });
+
+        //Listener Bouton regle
+        regle.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+
+
+            }
+        });
+
+        //Listener Bouton settings
+        settings.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+
+
+            }
+        });
+
+        //Listener Bouton pions
+        pions.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+
+
+            }
+        });
+
+        //Listener Bouton quit
+        quit.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+
+
+            }
+        });
+
+        //Listener Bouton passertour
+        passertour.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+
+
+            }
+        });
 
     }
 
